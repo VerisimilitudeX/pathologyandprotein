e3ff2e6497ea754c666b6d6407b946d97163fc64
Piyush Acharya <acharyapiyush1@outlook.com>
2024-06-28 15:45:24 -0700

    finish practice

---
+++ 1. Pre-Course Training/Introduction to Numpy/np_create.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def task1():
+    # Generate and return the following array: [3, 2, 5, 6]
+    return np.array([3, 2, 5, 6])
+    
+
+def task2():
+    # TODO: Generate and return an array of the cubes of every integer 
+    # from 3 to 9 (inclusive).
+    # HINT: remember we can easily generate lists using list comprehension
+    return np.array([x**3 for x in range(3, 10)])
+    
+    
+
+def task3():
+    # TODO: Generate and return an array of every 3rd number 
+    # from 10 to 20 (inclusive)
+    return np.array([x for x in range(10, 20, 3)])
+    
+
+def main():
+    print(task1())
+    print(task2())
+    print(task3())
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CSE416
  
+My coursework for CSE 416: Introduction to Machine Learning at the University of Washington. Taken in Summer '24.
