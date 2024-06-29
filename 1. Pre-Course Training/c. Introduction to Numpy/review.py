5b0131555297092f6b5e2654b78f5255adfc54c3
Piyush Acharya <acharyapiyush1@outlook.com>
2024-06-28 19:05:28 -0700

    finish review

--- 1. Pre-Course Training/c. Introduction to Numpy/review.py
+++ 1. Pre-Course Training/c. Introduction to Numpy/review.py
@@ -3,13 +3,13 @@ import numpy as np
 temperatures = np.genfromtxt('temperature_data.csv', delimiter=',')
 
 # TODO
-temperatures_fixed = 
+temperatures_fixed = temperatures + 3
 
 # TODO
-monday_temperatures =
+monday_temperatures = temperatures_fixed[0]
 
 # TODO
-thursday_friday_morning = 
+thursday_friday_morning = temperatures_fixed[3:5, 1]
 
 # TODO
-temperature_extremes = 
+temperature_extremes = temperatures_fixed[(temperatures_fixed < 50) | (temperatures_fixed > 60)]
