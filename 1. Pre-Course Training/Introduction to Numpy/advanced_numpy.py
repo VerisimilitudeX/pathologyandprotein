249147cc617001d65b36520c6ababe6cd773f294
Piyush Acharya <acharyapiyush1@outlook.com>
2024-06-28 17:58:29 -0700

    progress so far

---
+++ 1. Pre-Course Training/Introduction to Numpy/advanced_numpy.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# HINT: How can you use the zeros method to do this?
+def task1():
+    return np.zeros((3, 4), dtype=int) + 3
+    
+
+def task2():
+    array2 = np.array([x for x in range(1, 12)])
+    return array2.reshape(3, 4)
+
+# HINT: Using a list might help you here.
+def task3():
+    
+    
+
+def main():
+    print(task1())
+    print(task2())
+    print(task3())
+
+if __name__ == "__main__":
+    main()
