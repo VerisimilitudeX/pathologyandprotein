78816aa5269d5b136924b2cfc8220a985cde07fc
Piyush Acharya <acharyapiyush1@outlook.com>
2024-06-28 18:26:53 -0700

    reorganize files

---
+++ 1. Pre-Course Training/c. Introduction to Numpy/review.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+temperatures = np.genfromtxt('temperature_data.csv', delimiter=',')
+
+# TODO
+temperatures_fixed = 
+
+# TODO
+monday_temperatures =
+
+# TODO
+thursday_friday_morning = 
+
+# TODO
+temperature_extremes = 
